a8d59db729c9041e3cc2ddd3669d0c90b942e941
Joel Chacón Castillo <joel.chacon@cimat.mx>
2025-11-11 15:42:17 -0600

    Delete SteinerTreeProblem_v1 directory

--- SteinerTreeProblem_v1/V4.exact/utils.h
+++
@@ -1,2 +0,0 @@
-int getRandomInteger0_N(int n);//get random integer [0, n]
-double generateRandomDouble0_Max(double maxValue);
--- SteinerTreeProblem_v1/V4/utils.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-int getRandomInteger0_N(int n){ 
-  return (int) ((n + 1.0)*rand()/(RAND_MAX+1.0));
-}
-
-double generateRandomDouble0_Max(double maxValue){
-	return (double)(rand()) / RAND_MAX * maxValue;
-}
